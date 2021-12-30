67ccb4449dab3df3b594a7eb4ac0e5e257fc55bc
SaigyoujiYuyuko <hgk-saigyoujiyuyuko@outlook.com>
2021-12-30 22:30:37 +0800

    change spect settings

--- util/spectrogram.py
+++ util/spectrogram.py
@@ -6,9 +6,9 @@ import numpy as np
 def get_spectrogram(audio_clip, rate):
     # 频谱图
     waveform = tf.cast(audio_clip, tf.float32)
-    spectrogram = tfio.audio.spectrogram(audio_clip, nfft=1024, window=512, stride=256)
+    spectrogram = tfio.audio.spectrogram(audio_clip, nfft=1024, window=1024, stride=512)
     spectrogram = tfio.audio.melscale(spectrogram, rate=rate, mels=1024, fmin=0, fmax=rate / 2)
-    spectrogram = tfio.audio.dbscale(spectrogram, top_db=50)
-    spectrogram = tfio.audio.freq_mask(spectrogram, param=240)
+    spectrogram = tfio.audio.dbscale(spectrogram, top_db=70)
+    # spectrogram = tfio.audio.freq_mask(spectrogram, param=240)
 
     return spectrogram
