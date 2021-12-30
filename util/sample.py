3562ee86054e86c0cd9015c4f2167d165970362c
SaigyoujiYuyuko <hgk-saigyoujiyuyuko@outlook.com>
2021-12-30 22:31:19 +0800

    修改解析音频方法

--- util/sample.py
+++ util/sample.py
@@ -5,13 +5,13 @@ import matplotlib.pyplot as plt
 
 
 def gen_train_sample(file, label, clip_size):
-    audio_tensor, audio_mono = util.audio.parse_audio(file)
-    clips = int(audio_tensor.shape[0] / clip_size)
+    rate, audio_mono = util.audio.parse_audio(file)
+    clips = int(audio_mono.shape[0] / clip_size)
 
     spect = []
     labels = []
     for seg in range(0, clips):
-        spect.append(spectrogram.get_spectrogram(audio_mono[seg * clip_size: seg * clip_size + clip_size], audio_tensor.rate.numpy()))
+        spect.append(spectrogram.get_spectrogram(audio_mono[seg * clip_size: seg * clip_size + clip_size], rate))
         labels.append(label)
 
     return spect, labels
