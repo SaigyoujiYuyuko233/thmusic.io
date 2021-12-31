5ddd8f45603f5590764d557a5d0712f56e8ffef2
SaigyoujiYuyuko <hgk-saigyoujiyuyuko@outlook.com>
2021-12-31 19:19:24 +0800

    添加了更多频谱图的优化

--- spectrogram-demo.py
+++ spectrogram-demo.py
@@ -2,31 +2,39 @@ import tensorflow as tf
 import tensorflow_io as tfio
 import numpy as np
 import matplotlib.pyplot as plt
+from util import audio
 
-CLIP_SIZE = 480000
+CLIP_SIZE = 240000
 if __name__ == '__main__':
     # Read
-    audio = tfio.audio.AudioIOTensor("./sakurasakura-demo/dataset/train/sakurasakura/sakurasakura-origion.mp3", dtype=tf.dtypes.float32)
+    rate, audio_mono = audio.parse_audio("./sakurasakura-demo/dataset-compressed/prediction/sakurasakura - 狐の工作室.mp3")
 
     # 转换为单声道
-    audio_mono = tf.convert_to_tensor(np.sum(audio[:].numpy(), axis=1) / 2.0, dtype=tf.dtypes.float32)
-    audio_mono = tf.reshape(audio_mono, shape=[len(audio_mono), 1])
-    audio_mono = tf.squeeze(audio_mono, axis=[-1])
+    # audio_mono = tf.convert_to_tensor(np.sum(audio[:].numpy(), axis=1) / 2.0, dtype=tf.dtypes.float32)
+    # audio_mono = tf.reshape(audio_mono, shape=[len(audio_mono), 1])
+    # audio_mono = tf.squeeze(audio_mono, axis=[-1])
 
     # 片段
     audio_mid = int(len(audio_mono) / 2)
     clip_mid = int(CLIP_SIZE / 2)
     audio_clip = audio_mono[audio_mid - clip_mid: audio_mid + clip_mid]
 
-    # audio_enc = tfio.util.encode_mp3(tf.reshape(audio_clip, shape=[len(audio_clip), 1]), rate=util.rate.numpy())
+    # audio_enc = tfio.audio.encode_mp3(tf.reshape(audio_clip, shape=[len(audio_clip), 1]), rate=rate)
     # tf.io.write_file("processed.mp3", audio_enc)
 
     # 频谱图
     waveform = tf.cast(audio_clip, tf.float32)
-    spectrogram = tfio.audio.spectrogram(audio_clip, nfft=1024, window=512, stride=256)
-    spectrogram = tfio.audio.melscale(spectrogram, rate=audio.rate.numpy(), mels=1024, fmin=0, fmax=audio.rate.numpy() / 2)
-    spectrogram = tfio.audio.dbscale(spectrogram, top_db=50)
-    spectrogram = tfio.audio.freq_mask(spectrogram, param=240)
+    spectrogram = tfio.audio.spectrogram(audio_clip, nfft=2048, window=800, stride=1024)
+    spectrogram = tfio.audio.melscale(spectrogram, rate=rate, mels=240, fmin=0, fmax=rate / 2)
+    spectrogram = tfio.audio.dbscale(spectrogram, top_db=20)
+    print(spectrogram.shape)
+    spectrogram = spectrogram.numpy()
 
-    plt.imshow(spectrogram.numpy())
+    # mask
+    for i in range(0, 20):
+        spectrogram[:, i] = np.zeros(len(spectrogram))
+    spectrogram = tf.convert_to_tensor(spectrogram)
+
+    print(spectrogram)
+    plt.imshow(tf.math.log(spectrogram).numpy())
     plt.show()
