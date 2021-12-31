ba32c5b459451f91b2eb1a79e16f0be988eaf82d
SaigyoujiYuyuko <hgk-saigyoujiyuyuko@outlook.com>
2021-12-31 19:35:44 +0800

    优化声谱图

--- util/spectrogram.py
+++ util/spectrogram.py
@@ -6,9 +6,14 @@ import numpy as np
 def get_spectrogram(audio_clip, rate):
     # 频谱图
     waveform = tf.cast(audio_clip, tf.float32)
-    spectrogram = tfio.audio.spectrogram(audio_clip, nfft=1024, window=1024, stride=512)
-    spectrogram = tfio.audio.melscale(spectrogram, rate=rate, mels=1024, fmin=0, fmax=rate / 2)
-    spectrogram = tfio.audio.dbscale(spectrogram, top_db=70)
-    # spectrogram = tfio.audio.freq_mask(spectrogram, param=240)
+    spectrogram = tfio.audio.spectrogram(audio_clip, nfft=2048, window=800, stride=1024)
+    spectrogram = tfio.audio.melscale(spectrogram, rate=rate, mels=220, fmin=0, fmax=rate / 2)
+    spectrogram = tfio.audio.dbscale(spectrogram, top_db=28)
+
+    # mask
+    spectrogram = np.delete(spectrogram, range(0, 10), axis=1)
+    for i in range(0, 24):
+        spectrogram[:, i] = np.zeros(len(spectrogram))
+    spectrogram = tf.convert_to_tensor(spectrogram)
 
     return spectrogram
