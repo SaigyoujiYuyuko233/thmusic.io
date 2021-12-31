a12ed0cdb1513b84470dd7c685e3f8c6865a5332
SaigyoujiYuyuko <hgk-saigyoujiyuyuko@outlook.com>
2021-12-31 11:30:07 +0800

    fix: 双声道转换

--- util/audio.py
+++ util/audio.py
@@ -13,6 +13,7 @@ def parse_audio(file):
     audio_mono = np.float32(audio_mono) / 2 ** 15
 
     # 转换为单声道
+    audio_mono = tf.convert_to_tensor(np.sum(audio_mono, axis=1) / 2.0, dtype=tf.dtypes.float32)
     audio_mono = tf.reshape(audio_mono, shape=[len(audio_mono), 1])
     audio_mono = tf.squeeze(audio_mono, axis=[-1])
 
