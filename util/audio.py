88b09c8c1df0c490c76c42b9a0ec5c612e3d2383
SaigyoujiYuyuko <hgk-saigyoujiyuyuko@outlook.com>
2021-12-31 11:45:58 +0800

    fix: 单声道无法合并

--- util/audio.py
+++ util/audio.py
@@ -10,10 +10,10 @@ def parse_audio(file):
     audio_mono = np.array(audio.get_array_of_samples())
     if audio.channels == 2:
         audio_mono = audio_mono.reshape((-1, 2))
+        audio_mono = tf.convert_to_tensor(np.sum(audio_mono, axis=1) / 2.0, dtype=tf.dtypes.float32)
     audio_mono = np.float32(audio_mono) / 2 ** 15
 
     # 转换为单声道
-    audio_mono = tf.convert_to_tensor(np.sum(audio_mono, axis=1) / 2.0, dtype=tf.dtypes.float32)
     audio_mono = tf.reshape(audio_mono, shape=[len(audio_mono), 1])
     audio_mono = tf.squeeze(audio_mono, axis=[-1])
 
